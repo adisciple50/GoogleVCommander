b774da1c3e6f71c48bdad06bd972237c849b338a
Jason Crockett <deddokatana@gmail.com>
2015-08-02 23:26:57 +0100

    Easy Project Setup made

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(
+    name='GoogleVCommander',
+    version='0.1',
+    packages=[''],
+    url='https://github.com/deddokatana/GoogleVCommander',
+    license='http://opensource.org/licenses/MIT - MIT Licence',
+    author='Jason Crockett; Ben Cullen',
+    author_email='deddokatana@gmail.com;ancient.sun.ra@gmail.com',
+    description='Hackable Voice Commands for your computer',
+    install_requires = ['SpeechRecognition']
+)
